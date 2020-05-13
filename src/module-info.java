9fa4e9df584b8ba6680fccab140ac2e0939e4d43
mari_s <ms.mari.suzuki@gmail.com>
2020-05-13 19:24:25 +0900

    in progress 187.Controls - fixed the exception in application start error by re-creating module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,10 @@
 module Controls {
-
     requires javafx.fxml;
     requires javafx.controls;
+    requires jlfgr;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.media;
 
     opens sample;
 }
